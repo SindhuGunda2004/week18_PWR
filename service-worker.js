9a773886287286b6712e524d560715a3a9c98764
SindhuGunda24 <sindhugunda1592@gmail.com>
2024-02-20 09:57:09 +0400

    c4

--- service-worker.js
+++ service-worker.js
@@ -19,22 +19,37 @@ var cacheFile = [
 // what is service worker 
 // addall helps to get all files to 
 
-self.addEventListener("install", (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFile);
-        })
-    );
-});
+// self.addEventListener("install", (e) => {
+//     console.log('[Service Worker] Install');
+//     e.waitUntil(
+//         caches.open(cacheName).then((cache) => {
+//             console.log('[Service Worker] Caching all the files');
+//             return cache.addAll(cacheFile);
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fecthing Resource: ' + e.request.url);
+//             // r is matching file if it exists in the cache 
+//             return r
+//         })
+//     );
+// });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
             console.log('[Service Worker] Fecthing Resource: ' + e.request.url);
             // r is matching file if it exists in the cache 
-            return r 
+            return r || fetch(e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
