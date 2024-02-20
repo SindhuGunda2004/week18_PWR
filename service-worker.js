eec4b5fec5ebff1abd94b61a8c0ec315ad78d970
SindhuGunda24 <sindhugunda1592@gmail.com>
2024-02-20 09:32:21 +0400

    ss

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+var cacheName = "petstore-v1";
+var cacheFile = [
+    "Pet_depot.html",
+    "products.js",
+    "Pet_depot.css",
+    "Pet_depot.webmanifest",
+    "images/catfood.jpeg",
+    "images/catfood2.png",
+    "images/catfood3.png",
+    "images/kittyfood.png",
+    "images/icon-store-512.png",
+    "images/icon-store-32.png"
+];
